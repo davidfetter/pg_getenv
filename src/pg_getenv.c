4e29a8de1336485a0a603606784423fd093b59dd
David Fetter <david@fetter.org>
2021-08-16 17:59:39 -0700

    Now presents the environment
    ...once.
    
    In the process,
    - Changed to materialize mode per Marko Tiikkaja to do the result in one shot.
    - Used session_auth_is_superuser per David Christensen to prevent non-superusers from executing the function no matter what.
    
    XXX This crashes the second time it's called. Could it be that a deep
    copy of the environment needs to be made in order not to scribble on
    things that shouldn't be scribbled upon? A const decoration somewhere?

--- src/pg_getenv.c
+++ src/pg_getenv.c
@@ -1,8 +1,10 @@
 #include "postgres.h"
 
+#include "access/tupdesc.h"
 #include "fmgr.h"
 #include "funcapi.h"
-#include "access/tupdesc.h"
+#include "utils/builtins.h"
+#include "utils/guc.h"
 
 PG_MODULE_MAGIC;
 
@@ -14,77 +16,66 @@ extern char **environ;
 Datum
 pg_getenv(PG_FUNCTION_ARGS)
 {
-	FuncCallContext	*funcctx;
-	int				 call_cntr,
-					 max_calls;
-	TupleDesc		 tupdesc;
-	AttInMetadata	*attinmeta;
+	/* Input parameters (PG_GETARG_* go here, but not yet */
+
+	ReturnSetInfo	   *rsinfo  =(ReturnSetInfo *) fcinfo->resultinfo;
+	bool				randomAccess;
+	int					maxKBytes = 1024;
+	TupleDesc			tupdesc;
+	Tuplestorestate	   *tupstore;
+	MemoryContext		oldcontext;
 
 	char		   **env = environ;
 
-	if (SRF_IS_FIRSTCALL())
+	/* Must be superuser because duh */
+	if (!session_auth_is_superuser)
+		ereport(ERROR,
+				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				 errmsg("must be superuser to see server environment variables")));
+
+	/* It's a SRF, so kinda need to be that */
+	if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("set-valued function called in context that cannot accept a set")));
+
+	/* Can we actually return a tuplestore? */
+	if (!(rsinfo->allowedModes & SFRM_Materialize))
+		ereport(ERROR,
+				(errcode(ERRCODE_SYNTAX_ERROR),
+				 errmsg("materialize mode required, but not allowed in this context")));
+
+	oldcontext = MemoryContextSwitchTo(rsinfo->econtext->ecxt_per_query_memory);
+
+	if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+		elog(ERROR, "return type must be a row type");
+
+	randomAccess = (rsinfo->allowedModes & SFRM_Materialize_Random) != 0;
+	tupstore = tuplestore_begin_heap(randomAccess, false, maxKBytes);
+	rsinfo->returnMode = SFRM_Materialize;
+	rsinfo->setResult = tupstore;
+	rsinfo->setDesc = tupdesc;
+
+	MemoryContextSwitchTo(oldcontext);
+
+	/* Get everything out of the environment and into tuples */
+	while (*env)
 	{
-		MemoryContext	oldcontext;
-		funcctx = SRF_FIRSTCALL_INIT();
+		Datum		values[2];
+		bool		nulls[2];
+		char	   *ptr;
+		char	   *pair = *env;
+		char	   *name, *value;
 
-		oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+		name = strtok_r(pair, "=", &ptr);
+		value = strtok_r(NULL, "=", &ptr);
+		env++;
 
-		funcctx->max_calls = PG_GETARG_UINT32(0);
+		values[0] = CStringGetTextDatum(name);
+		values[1] = CStringGetTextDatum(value);
 
-		if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
-			ereport(ERROR,
-					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-					 errmsg("function returning record called in context "
-							" that cannot accept type record")));
-
-		attinmeta = TupleDescGetAttInMetadata(tupdesc);
-		funcctx->attinmeta = attinmeta;
-
-		MemoryContextSwitchTo(oldcontext);
+		tuplestore_putvalues(tupstore, tupdesc, values, nulls);
 	}
 
-	funcctx = SRF_PERCALL_SETUP();
-
-	call_cntr = funcctx->call_cntr;
-	max_calls = funcctx->max_calls;
-	attinmeta = funcctx->attinmeta;
-
-	if (call_cntr < max_calls && *env)
-	{
-		char		**values;
-		HeapTuple	  tuple;
-		Datum		  result;
-		char		 *name, *value;
-		char		 *e = *env++;
-		char		 *eq;
-
-		/* Get what's on either side of the first equals sign into n and v. */
-		eq = strchr(e, '=');
-		*eq = '\0';
-		name = e;
-		value = eq + 1;
-
-		values = (char **) palloc(2 * sizeof(char *));
-		values[0] = (char *) palloc(256 * sizeof(char));
-		values[1] = (char *) palloc(256 * sizeof(char));
-
-		snprintf(values[0], 256, "%s", name);
-		snprintf(values[1], 256, "%s", value);
-
-		/* Build tuple */
-		tuple = BuildTupleFromCStrings(attinmeta, values);
-
-		/* Turn it into a datum */
-		result = HeapTupleGetDatum(tuple);
-
-		pfree(values[0]);
-		pfree(values[1]);
-		pfree(values);
-
-		SRF_RETURN_NEXT(funcctx, result);
-	}
-	else
-	{
-		SRF_RETURN_DONE(funcctx);
-	}
+	return (Datum) 0;
 }
